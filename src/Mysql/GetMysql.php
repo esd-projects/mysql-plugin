1d28c263f9c37eb7137fd494d4a0251e2a7ba19e
zhangjincheng <zhangjincheng@huidr.com>
2019-04-28 17:37:59 +0800

    mysql插件

--- src/Mysql/GetMysql.php
+++ src/Mysql/GetMysql.php
@@ -18,11 +18,16 @@ trait GetMysql
      */
     public function mysql($name = "default")
     {
-        $mysqlPool = getDeepContextValueByClassName(MysqlManyPool::class);
-        if ($mysqlPool instanceof MysqlManyPool) {
-            return $mysqlPool->getPool($name)->db();
-        }else{
-            throw new MysqlException("没有找到名为{$name}的mysql连接池");
+        $db = getContextValue("MysqliDb:$name");
+        if ($db == null) {
+            $mysqlPool = getDeepContextValueByClassName(MysqlManyPool::class);
+            if ($mysqlPool instanceof MysqlManyPool) {
+                $db = $mysqlPool->getPool($name)->db();
+                setContextValue("MysqliDb:$name", $db);
+                return $db;
+            } else {
+                throw new MysqlException("没有找到名为{$name}的mysql连接池");
+            }
         }
     }
 }
